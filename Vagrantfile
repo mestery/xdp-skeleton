32c108f5c0d33220dfdc0423413c53d6ad242103
Kyle Mestery <mestery@mestery.com>
2021-04-29 08:24:30 -0500

    Vagrant: Add Vagrantfile and startup script
    Add a Vagrantfile and bootstrap.sh to configure the initial VM. While
    here, also add a .gitignore to ignore the .vagrant directory.
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

---
+++ Vagrantfile
@@ -0,0 +1,20 @@
+#
+# NOTE: This script requires the reload pluging, install it as follows:
+#         vagrant plugin install vagrant-reload
+#
+
+Vagrant.configure("2") do |config|
+
+  config.vm.provider "virtualbox" do |v|
+    v.name = "xdp-skeleton"
+    v.cpus = 6
+    v.memory = 16384
+  end
+
+  config.vm.box = "kwilczynski/ubuntu-20.04-docker"
+  config.vm.hostname = "xdp-skeleton"
+
+  config.vm.synced_folder ".", "/git"
+  config.vm.provision :shell, privileged: false, path: "bootstrap.sh"
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
+            make \
+            gcc \
+            clang \
+            git \
+            pkg-config \
+            autoconf \
+            automake \
+            gettext \
+            libtool \
+            libev-dev \
+            libpcre3-dev \
+            libudns-dev \
+            debhelper \
+            dh-autoreconf \
+            dpkg-dev \
+            procps \
+            git \
+            cmake \
+            clang \
+            libclang-dev \
+            libelf-dev \
+            llvm-dev \
+            flex \
+            bison \
+            cmake \
+            python \
+            libpcap-dev \
+            python3 \
+            python3-dev \
+            python3-setuptools \
+            python3-virtualenv \
+            python3-venv \
+            python3-pip \
+            linux-tools-common \
+            libdw1
+
+# Set some default network buffer values
+sudo sysctl -w net.core.rmem_default=134217728
+sudo sysctl -w net.core.rmem_max=268435456
+sudo sysctl -w net.core.wmem_default=134217728
+sudo sysctl -w net.core.wmem_max=268435456
+sudo sysctl -w net.ipv4.tcp_rmem="134217728 134217728 134217728"
+sudo sysctl -w net.ipv4.tcp_wmem="134217728 134217728 134217728"
+sudo sysctl -w net.ipv4.tcp_mem="134217728 134217728 134217728"
+sudo sysctl -w net.ipv4.udp_mem="134217728 134217728 134217728"
+sudo sysctl -w net.ipv4.udp_rmem_min=134217728
+sudo sysctl -w net.ipv4.udp_wmem_min="134217728 134217728 134217728"
+
+# Update pahole
+wget http://archive.ubuntu.com/ubuntu/pool/universe/d/dwarves-dfsg/dwarves_1.17-1_amd64.deb
+DEBIAN_FRONTEND=noninteractive sudo apt-get install ./dwarves_1.17-1_amd64.deb
